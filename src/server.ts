8898d3696ba4068de1ab0956768dd85a268435b9
abduvalee95 <abduvalee95@gmail.com>
2024-02-22 21:12:56 +0900

    BRR: Initial Setup

---
+++ src/server.ts
@@ -0,0 +1,71 @@
+// **                                   Typescript 
+// Typescript codelirimizni Compiler qilib beradi oz vaqrida Error chiqadigon qiladi
+// variableni ohiriga : va dynamic type ni korsatishimiz kk boladi
+let box: string = "hello"
+// box = 234,
+box = "hi"
+
+//number type
+const counter: number = 243
+// counter = "234" // hato int qabbul qiladi
+
+//ham number ham string qabul qiladigonni yozadigon bolsak
+let stage: number | string 
+stage = "hello",
+stage = 23
+
+// boolean
+let boo: true | false 
+boo = true
+boo = false
+
+// object misolida interface type uchun hizmat qiladigon bir soya
+interface Person {
+    name: string,
+    age: number,
+    work: boolean,
+    lastname: null;
+}
+
+const person: Person =  {
+    name: 'John',
+    age: 23,
+    work:true,
+    lastname: null,
+}
+
+// **                               array bn 
+
+let skills: string []; // faqat string qabul qiladi
+skills = ['Problem', 'solving','machine']
+
+
+let skills2: (string | number)[]; // faqat string yoki number
+skills2 = ['Problem', 'solving','machine', 34332]
+
+class Person1 {
+    age:number;
+    name:string;
+    lastname:string;
+    work:boolean
+
+    constructor(name:string,age:number,lastname:string,work:boolean) {
+        this.age = age;
+        this.name = name;
+        this.lastname = lastname;
+        this.work = work;
+    }
+}
+// togri kiritmaguncha run bolmidi Compile error bolib turaveradi hatoni togrilamaguncha
+const per = new Person1('John', 33,  'wick', false);
+console.log(per);
+console.log("EXUCUTED");
+
+const count:number = 100;
+
+import moment from 'moment';
+
+
+const currentTime  = moment().format("yyyy mm dd hh:mm");
+console.log(currentTime);
+
