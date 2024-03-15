7f218596835fbd3b2b165f3936955b0ad819824f
abduvalee95 <abduvalee95@gmail.com>
2024-03-15 11:58:06 +0900

    Fix: getAllProducts api and passing data into ejs

--- src/models/Product.service.ts
+++ src/models/Product.service.ts
@@ -17,6 +17,23 @@ class ProductService {
 
 //*                             SSR
 
+
+public async getAllProducts(
+  ): Promise<Product[]> {
+
+      const result = await this.productModel.find()
+      .exec();
+      if(!result) throw new Errors(HttpCode.NOT_MODIFIED,Message.UPDATE_FAILED);
+
+      // console.log(result);
+      return result
+      
+  }
+
+
+
+
+
 public async createNewProduct(input:ProductInput): Promise<Product> {
     try {
         return await this.productModel.create(input);
