ad48106a44f17b10399b935763b1a68980a9803d
abduvalee95 <abduvalee95@gmail.com>
2024-09-16 16:01:00 +0900

    feat: prepare for production

--- src/server.ts
+++ src/server.ts
@@ -1,30 +1,21 @@
-import dotenv from "dotenv";
-dotenv.config();
-import mongoose from "mongoose";
-import server from "./app";
-
-mongoose.connect(process.env.MONGO_URL as string, {}) //{} conection option 
-.then((data) => {
-    console.log("MongoDB connection succsess");
-    const PORT = process.env.PORT ?? 3003;
-    server.listen(PORT, function() {
-        console.info(`The server is running successfully on port: ${PORT}`);
-        console.info(`Admin project on http://localhost:${PORT} \n`)
-    })
+import dotenv from 'dotenv'
+dotenv.config({
+	path: process.env.NODE_ENV === 'production' ? '.env.production' : '.env',
 })
+import mongoose from 'mongoose'
+import server from './app'
+mongoose
+	.connect(process.env.MONGO_URL as string, {}) //{} conection option
+	.then(data => {
+		console.log('MongoDB connection succsess')
+		const PORT = process.env.PORT ?? 3003
+		server.listen(PORT, function () {
+			console.info(`The server is running successfully on port: ${PORT}`)
+			console.info(`Admin project on http://localhost:${PORT} \n`)
+		})
+	})
 
-.catch((err)=>
-console.log("ERROR on connection PORT",err)
-)
-
-
-
-
-
-
-
-
-
+	.catch(err => console.log('ERROR on connection PORT', err))
 
 /* 
 // **                                   Typescript 
